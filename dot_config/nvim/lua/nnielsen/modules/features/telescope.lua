d74244586880910a4a2a98fcb26719be8c50ed43
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2023-03-04 16:18:37 +0100

    nvim: update branch on telescope plugin

--- dot_config/nvim/lua/nnielsen/modules/features/telescope.lua
+++ dot_config/nvim/lua/nnielsen/modules/features/telescope.lua
@@ -5,7 +5,7 @@ telescope.packages = {
 
 	["telescope"] = {
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
+		branch = "0.1.x",
 		requires = "nvim-lua/plenary.nvim",
 	},
 	["telescope-fzf-native"] = {
