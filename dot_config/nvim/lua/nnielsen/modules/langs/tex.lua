c1ce2506c9e0e0431eafc92aed215aecca3815fe
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2023-03-10 13:08:16 +0100

    nvim: fix vim.g

--- dot_config/nvim/lua/nnielsen/modules/langs/tex.lua
+++ dot_config/nvim/lua/nnielsen/modules/langs/tex.lua
@@ -1,3 +1,4 @@
+local g = vim.g
 ---@type Module
 local tex = {}
 
@@ -11,22 +12,20 @@ tex.packages = {
 tex.configs = {
 	["vimtex"] = function()
 		-- Use Skim as viewer application for vimtex
-		--let g:vimtex_view_method='skim'
+		g.vimtex_view_method = "skim"
 		-- Disable custom warnings based on regexp
-		--let g:vimtex_quickfix_ignore_filters = [
-		--\ 'Font shape declaration has incorrect series value',
-		--\ 'Overfull \\hbox',
-		--\ 'Marginpar on page',
-		--\ 'No \\printglossary',
-		--\ 'You have requested document class',
-		--\ 'The length marginparwidth is less than',
-		--\ 'Draft mode on'
-		--\ ]
+		g.vimtex_quickfix_ignore_filters = {
+			"Font shape declaration has incorrect series value",
+			"Overfull \\hbox",
+			"Marginpar on page",
+			"No \\printglossary",
+			"You have requested document class",
+			"The length marginparwidth is less than",
+			"Draft mode on",
+		}
 		--let g:vimtex_subfile_start_local = 1
 		--let g:vimtex_fold_enabled = 1
-
-		--configure how latexmk is called by vimtex
-		--let g:vimtex_compiler_latexmk =
+		g.vimtex_fold_enabled = 1
 	end,
 }
 
