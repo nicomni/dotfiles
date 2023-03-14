72142d768cd6e00654518963efda5927b70024da
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2023-03-14 19:46:47 +0100

    nvim: exchange vim-test for neotest

--- dot_config/nvim/lua/nnielsen/modules/features/testing.lua
+++ dot_config/nvim/lua/nnielsen/modules/features/testing.lua
@@ -2,7 +2,29 @@
 local testing = {}
 
 testing.packages = {
-  ["vim-test"] = { "vim-test/vim-test" },
+	--[[ ["vim-test"] = {
+		"vim-test/vim-test",
+		requires = "tpope/vim-dispatch",
+	}, ]]
+	["neotest"] = {
+		"nvim-neotest/neotest",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-neotest/neotest-python",
+		},
+	},
+}
+
+testing.configs = {
+	["neotest"] = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python"),
+			},
+		})
+	end,
 }
 
 return testing
