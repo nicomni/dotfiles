fd7afe1555a184175b0bce2fada8ba288dd18b54
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2023-11-22 18:34:15 +0100

    nvim: change colorscheme -> gruvbox

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,13 @@ return {
       on_colors = function(colors)
         colors.border = colors.orange
       end,
-    }
-  }
+    },
+  },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
