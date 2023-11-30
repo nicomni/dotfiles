9d634a1a83e36207828458d820c9d3fdbf745f8d
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2023-11-30 12:13:35 +0100

    nvim: remove user config for treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, _)
-      vim.opt.foldmethod = 'expr'
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end
-  }
-}
