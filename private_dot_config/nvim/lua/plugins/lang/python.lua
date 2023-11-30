6a4415e8377040d61bb89ce26a03092f80b1dd83
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2023-11-30 12:14:39 +0100

    nvim: user config for python

---
+++ private_dot_config/nvim/lua/plugins/lang/python.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "linux-cultist/venv-selector.nvim",
+    version = false,
+    opts = {
+      anaconda_base_path = "~/miniconda3",
+      anaconda_envs_path = "~/miniconda3/envs",
+    },
+  },
+}
