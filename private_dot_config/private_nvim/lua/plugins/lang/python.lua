939a5ac02b0f7ffa0ab28eb5a995872297cdaf66
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-03-02 23:12:41 +0100

    nvim(python): update config

--- private_dot_config/private_nvim/lua/plugins/lang/python.lua
+++ private_dot_config/private_nvim/lua/plugins/lang/python.lua
@@ -1,10 +1,30 @@
 return {
-  {
-    "linux-cultist/venv-selector.nvim",
-    version = false,
-    opts = {
-      anaconda_base_path = "~/miniconda3",
-      anaconda_envs_path = "~/miniconda3/envs",
-    },
-  },
+	{
+		"linux-cultist/venv-selector.nvim",
+		version = false,
+		opts = {
+			anaconda_base_path = "~/miniforge",
+			anaconda_envs_path = "~/miniforge/envs",
+		},
+	},
+	{
+		"tmhedberg/SimpylFold",
+		ft = "python",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		-- opts = {
+		--   servers = {
+		--     pyright = {
+		--       settings = {
+		--         python = {
+		--           analysis = {
+		--             diagnosticMode = "workspace",
+		--           },
+		--         },
+		--       },
+		--     },
+		--   },
+		-- },
+	},
 }
