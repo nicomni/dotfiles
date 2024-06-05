e733bbe0f82cbacd893ba9456c87362e33eba48c
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-06-05 20:47:07 +0200

    fix(neovim): fix a problem with copilot in Go files

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,14 @@
-local nls = require("null-ls")
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      server_opts_overrides = {
-        advanced = {
-          indentationMode = {
-            go = false,
-          },
-        },
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = {
-      sources = {},
-    },
-  },
+	{
+		"zbirenbaum/copilot.lua",
+		opts = {
+			server_opts_overrides = {
+				advanced = {
+					indentationMode = {
+						go = false,
+					},
+				},
+			},
+		},
+	},
 }
