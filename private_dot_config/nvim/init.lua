c0e0c238584ca3b74b24fd9d0f752435b6b14cf5
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-06-17 15:01:15 +0200

    feat(nvim): delegate LuaSnip to LazyVim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,6 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+local has_ls = pcall(require, "luasnip")
+if has_ls then
+	-- load my snippets
+	local lua_loader = require("luasnip.loaders.from_lua")
+	lua_loader.lazy_load({ paths = vim.list_extend({ "~/repos/snippetfiles/" }, vim.opt.rtp) })
+
+	-- add shelldoc snippets to shell files
+	local ls = require("luasnip")
+	ls.filetype_extend("zsh", { "shelldoc" })
+	ls.filetype_extend("sh", { "shelldoc" })
+end
+
 -- Add filetype mappings:
 vim.filetype.add({
 	extension = {
