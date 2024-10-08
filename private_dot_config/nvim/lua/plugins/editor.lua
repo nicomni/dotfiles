9fe4dbe0553ccf4ebd9df69146cfe113982b7ef9
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-10-08 19:16:19 +0200

    nvim: add custom options for neo-tree

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -5,4 +5,16 @@ return {
     "lewis6991/gitsigns.nvim",
     version = false,
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          always_show_by_pattern = {
+            "**/epic-react/v2/**/playground",
+          },
+        },
+      },
+    },
+  },
 }
