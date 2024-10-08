7557150c2d720c971855e3fcaca7b1d9eab5688c
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-10-08 19:14:24 +0200

    nvim: remove unnecessary telescope config

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -2,13 +2,6 @@
 ---@type LazyPluginSpec[]
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
-  {
     "lewis6991/gitsigns.nvim",
     version = false,
   },
