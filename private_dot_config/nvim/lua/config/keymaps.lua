7e89c997ed84991bf5913aeae3815dc7149bca95
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-10-08 19:29:58 +0200

    nvim: remove comments

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -4,19 +4,6 @@
 
 local wk = require("which-key")
 
--- local Util = require("lazyvim.util")
-
--- Default opts by which-key:
--- {
---   mode = "n", -- normal mode
---   -- prefix: use "<leader>f" for example for mapping everything related to finding files
---   -- the prefix is prepended to every mapping part of `mappings`
---   prefix = "",
---   buffer = nil, -- global mappings. specify a buffer number for buffer local mappings
---   silent = true, -- use `silent` when creating keymaps
---   noremap = true, -- use `noremap` when creating keymaps
---   nowait = false, -- use `nowait` when creating keymaps
--- }
 
 local keymaps = {
 	{
