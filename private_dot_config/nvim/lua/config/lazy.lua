b313bfce47b097f415502a7a26d2f4dc81e01043
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2024-10-10 22:32:40 +0200

    nvim: use latest git commit for plugins

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     -- version = false, -- always use the latest git commit
-    version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false, -- set to "*" to try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = false }, -- automatically check for plugin updates
