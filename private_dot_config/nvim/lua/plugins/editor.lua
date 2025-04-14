fb09e55873ba47601c4304e4ad03a6b51610aad9
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-04-14 14:57:31 +0200

    lazyvim: remove luasnip

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -1,32 +1,24 @@
 ---@module "lazy.types"
 ---@type LazyPluginSpec[]
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    version = false,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          always_show_by_pattern = {
-            "**/epic-react/v2/**/playground",
-          },
-        },
-      },
-    },
-    dependencies = {
-      "s1n7ax/nvim-window-picker",
-      name = "window-picker",
-    },
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    opts = {
-      load_ft_func = require("luasnip.extras.filetype_functions").extend_load_ft({
-        typescriptreact = { "typescript" },
-      }),
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		version = false,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					always_show_by_pattern = {
+						"**/epic-react/v2/**/playground",
+					},
+				},
+			},
+		},
+		dependencies = {
+			"s1n7ax/nvim-window-picker",
+			name = "window-picker",
+		},
+	},
 }
