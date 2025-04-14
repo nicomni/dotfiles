c14b492364ece108a137def27c4d1598bed573b6
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-04-14 14:58:22 +0200

    lazyvim: remove unused venv-selector

--- private_dot_config/nvim/lua/plugins/lang/python.lua
+++ private_dot_config/nvim/lua/plugins/lang/python.lua
@@ -1,11 +1,4 @@
 return {
-	-- {
-	--   "linux-cultist/venv-selector.nvim",
-	--   opts = {
-	--     anaconda_base_path = "~/miniforge",
-	--     anaconda_envs_path = "~/miniforge/envs",
-	--   },
-	-- },
 	{
 		"tmhedberg/SimpylFold",
 		ft = "python",
