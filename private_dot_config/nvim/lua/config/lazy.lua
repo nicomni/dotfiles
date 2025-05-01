819d6f3b0ea6bda5b2602f1c8f20ea05f7610ecc
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-05-01 18:46:09 +0200

    nvim: don't download habamax colorscheme

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -26,7 +26,7 @@ require("lazy").setup({
     -- version = false, -- always use the latest git commit
     version = false, -- set to "*" to try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
