63e42c8adb83f9f5bf1b96483f73d18eb16d07b8
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-05-26 17:49:22 +0200

    feat(nvim): update colorscheme

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,8 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "storm",
-      ---@param colors ColorScheme
-      on_colors = function(colors)
-        colors.border = colors.orange
-      end,
-    },
-  },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
 }
