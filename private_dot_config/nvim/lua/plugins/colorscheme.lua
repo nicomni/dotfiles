5cc1b01fa3c644aafd31e47c2ba1567c7b9067e2
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-07-03 18:09:59 +0200

    nvim(tokyonight): make customizations
    * Heavier window separator characters
    * More defined color of window separators

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
+---@module "tokyonight"
 ---@module "LazyVim"
 
 return {
@@ -9,5 +10,29 @@ return {
       -- different colorscheme.
       -- colorscheme = "",
     },
+  },
+  {
+    "folke/tokyonight.nvim",
+    optional = true,
+    ---@param opts tokyonight.Config
+    opts = function(_, opts)
+      -- Change characters used for separators (borders) between splits.
+      -- See ':help fillchars'
+      vim.opt.fillchars = {
+        horiz = "━",
+        horizup = "┻",
+        horizdown = "┳",
+        vert = "┃",
+        vertleft = "┫",
+        vertright = "┣",
+        verthoriz = "╋",
+      }
+      opts.on_highlights = function(hl, c)
+        -- Fix: default WinSeparator color is very dark and hard to see
+        hl.WinSeparator.fg = c.blue
+      end
+
+      return opts
+    end,
   },
 }
