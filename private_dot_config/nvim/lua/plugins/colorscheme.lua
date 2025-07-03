7c6c34fe83325d3e33c89d318d50db2c418f91a5
Nicolaj Magga Nielsen <nicolaj.niels1@gmail.com>
2025-07-03 18:09:06 +0200

    nvim(LazyVim): remove redundant colorscheme opt

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
+---@module "LazyVim"
+
 return {
   {
     "LazyVim/LazyVim",
+    ---@type LazyVimConfig
     opts = {
-      colorscheme = "tokyonight",
+      -- Lazyvim uses tokyonight. Uncomment the following line to use a
+      -- different colorscheme.
+      -- colorscheme = "",
     },
   },
 }
