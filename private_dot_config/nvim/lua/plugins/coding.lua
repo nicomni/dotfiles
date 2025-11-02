1505538a93f47a0537036576f5897854c422e420
Nicolaj M. Nielsen <nicolaj.niels1@gmail.com>
2025-11-02 18:34:39 +0100

    nvim: cmp menu: show source and LSP client name

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -1,11 +1,33 @@
+---@module "blink.cmp"
+
 return {
   {
     "saghen/blink.cmp",
+    ---@type blink.cmp.Config
     opts = {
       completion = {
         accept = {
           auto_brackets = {
             enabled = false,
+          },
+        },
+        menu = {
+          draw = {
+            -- Default value.
+            -- columns = { { "kind_icon" }, { "label", "label_description", gap = 1 } },
+            -- See: [Source on Github](https://github.com/saghen/blink.cmp/blob/2408f14f740f89d603cad33fe8cbd92ab068cc92/lua/blink/cmp/config/completion/menu.lua#L73-L73)
+            columns = {
+              { "kind_icon" },
+              { "label", "label_description", gap = 1 },
+              { "source_name", "lsp_client_name", gap = 1 },
+            },
+            components = {
+              lsp_client_name = {
+                text = function(ctx)
+                  return ctx.item.client_name or ""
+                end,
+              },
+            },
           },
         },
       },
